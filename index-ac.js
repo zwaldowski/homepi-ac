592e32dca15f1bcc57f1c5f85ffdebb5eab547b4
Zachary Waldowski <zach@waldowski.me>
2017-08-30 01:28:44 -0400

    Slightly generalize LIRC command

--- index-ac.js
+++ index-ac.js
@@ -1,27 +1,36 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const program = require('commander')
 
 program
+  .option('-d, --device <device>', 'Device name for LIRC')
   .arguments('<action>')
   .action(function(e){
       actionName = e;
-  }).parse(process.argv);
+  }).parse(process.argv)
+
+if (typeof program.device === 'undefined') {
+    console.error('Need device name for LIRC.')
+    process.exit(1)
+}
 
 if (typeof actionName === 'undefined' || !(/^(power|down|up|fan|timer|mode)$/i.test(actionName))) {
-    console.error('Need one of "power", "down", "up", "fan", "timer", or "mode".');
-    process.exit(1);
+    console.error('Need one of "power", "down", "up", "fan", "timer", or "mode".')
+    process.exit(1)
 }
 
 const lirc_node = require('lirc_node')
 lirc_node.init()
 
 const lircActions = {
-    'down': 'temperature_up',
-    'up': 'temperature_down',
-    'mode': 'ac_mode'
+    'power': 'KEY_POWER',
+    'down': 'BTN_GEAR_DOWN',
+    'up': 'BTN_GEAR_UP',
+    'fan': 'KEY_MOVE',
+    'timer': 'KEY_SLEEP',
+    'mode': 'KEY_MODE'
 };
 
 const lircAction = lircActions.hasOwnProperty(actionName) ? lircActions[actionName] : actionName
 
-lirc_node.irsend.send_once('LG_AKB73016012', lircAction)
+lirc_node.irsend.send_once(program.device, lircAction)
